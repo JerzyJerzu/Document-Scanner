1c710d1864c00ab184ab262cd854b8f3fc350f72
JerzyJerzu <PJ@pawlik.info.pl>
2023-12-31 02:08:48 +0100

    changed the 4 point tranform function

--- main.py
+++ main.py
@@ -2,54 +2,12 @@ import cv2
 import keyboard
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.spatial import distance as dist
 
 # to learn:
 # cv2.threshold
 # image restoration
 # optional: add the functionality to rotate a frame
-
-# this function is part of this github repository:
-#https://github.com/codegiovanni/Warp_perspective
-def four_point_transform(image,contour):
-    #TO DO:
-    points = contour.reshape(4, 2)
-    input_points = np.zeros((4, 2), dtype="float32")
-
-    #The sum of coordinates for each point in the points array is calculated.
-    #The top-left point is assigned the coordinates of the point with the smallest sum.
-    #The bottom-right point is assigned the coordinates of the point with the largest sum.
-
-    points_sum = points.sum(axis=1)
-    input_points[0] = points[np.argmin(points_sum)]
-    input_points[3] = points[np.argmax(points_sum)]
-
-    #The differences between coordinates for each point in the points array are calculated.
-    #The top-right point is assigned the coordinates of the point with the smallest difference.
-    #The bottom-left point is assigned the coordinates of the point with the largest difference.
-
-    points_diff = np.diff(points, axis=1)
-    input_points[1] = points[np.argmin(points_diff)]
-    input_points[2] = points[np.argmax(points_diff)]
-
-    (top_left, top_right, bottom_right, bottom_left) = input_points
-    bottom_width = np.sqrt(((bottom_right[0] - bottom_left[0]) ** 2) + ((bottom_right[1] - bottom_left[1]) ** 2))
-    top_width = np.sqrt(((top_right[0] - top_left[0]) ** 2) + ((top_right[1] - top_left[1]) ** 2))
-    right_height = np.sqrt(((top_right[0] - bottom_right[0]) ** 2) + ((top_right[1] - bottom_right[1]) ** 2))
-    left_height = np.sqrt(((top_left[0] - bottom_left[0]) ** 2) + ((top_left[1] - bottom_left[1]) ** 2))
-
-    # Output image size is just calculated as the biggest vertical and horizontal document contour
-    max_width = max(int(bottom_width), int(top_width))
-    max_height = max(int(right_height), int(left_height))
-    #max_height = int(max_width * 1.414)  # for A4
-
-    # Desired points values in the output image
-    converted_points = np.float32([[0, 0], [max_width, 0], [0, max_height], [max_width, max_height]])
-
-    # Perspective transformation
-    matrix = cv2.getPerspectiveTransform(input_points, converted_points)
-    img_output = cv2.warpPerspective(image, matrix, (max_width, max_height))
-
-    return img_output
 
 def detect_contour():
     blurred = cv2.GaussianBlur(frame, (5, 5), 1)
@@ -94,8 +52,77 @@ def thresholding(image):
 
     return threshold
 
+# This function comes from the Imutils package
+# https://github.com/PyImageSearch/imutils/blob/master/imutils/perspective.py
+def order_points(pts):
+    # sort the points based on their x-coordinates
+    xSorted = pts[np.argsort(pts[:, 0]), :]
+
+    # grab the left-most and right-most points from the sorted
+    # x-roodinate points
+    leftMost = xSorted[:2, :]
+    rightMost = xSorted[2:, :]
+
+    # now, sort the left-most coordinates according to their
+    # y-coordinates so we can grab the top-left and bottom-left
+    # points, respectively
+    leftMost = leftMost[np.argsort(leftMost[:, 1]), :]
+    (tl, bl) = leftMost
+
+    # now that we have the top-left coordinate, use it as an
+    # anchor to calculate the Euclidean distance between the
+    # top-left and right-most points; by the Pythagorean
+    # theorem, the point with the largest distance will be
+    # our bottom-right point
+    D = dist.cdist(tl[np.newaxis], rightMost, "euclidean")[0]
+    (br, tr) = rightMost[np.argsort(D)[::-1], :]
+
+    # return the coordinates in top-left, top-right,
+    # bottom-right, and bottom-left order
+    return np.array([tl, tr, br, bl], dtype="float32")
+
+#This function comes from the Imutils package
+#https://github.com/PyImageSearch/imutils/blob/master/imutils/perspective.py
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
+
 # change the argument to change the camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 _, frame = cap.read()
 
@@ -118,7 +145,7 @@ while True:
     # and use frame copy for saving the scan
     frame_copy = frame.copy()
 
-    document_contour = detect_contour()
+    scan_borders = detect_contour()
 
     #displaying the frame:
     plt.imshow(frame) #displays the current frame in the Matplotlib axis.
@@ -130,7 +157,8 @@ while True:
     if keyboard.is_pressed('esc'):
         break
     elif keyboard.is_pressed('w'):
-        transformed = four_point_transform(frame_copy, document_contour)
+        points = scan_borders.reshape(4, 2)
+        transformed = four_point_transform(frame_copy, points)
         #restored = thresholding(transformed)
         cv2.imwrite("scans/scan_" + str(scan_index) + ".jpg", transformed)
         scan_index += 1
